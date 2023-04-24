e8c65498fb5f94c1c1bd363e85f85edf4a213e6e
a.g.menshikov <a.g.menshikov@tinkoff.ru>
2023-04-24 12:06:07 +0300

    Initial Commit

---
+++ hackCoordinator/hackCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackCoordinatorApp.swift
+//  hackCoordinator
+//
+//  Created by Aleksandr Menshikov on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct hackCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
